c9fbf8e49239f3a6c81a83aea105848b62a3dcf3
Valery-a <ValeryKulchicki@gmail.com>
2023-09-29 02:15:58 +0300

    added world redraw to remove lag

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ def biome_color(value):
 
 def draw_world(offset_x, offset_y, zoom):
     world_surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     for y in range(SCREEN_HEIGHT):
         for x in range(SCREEN_WIDTH):
             noise_val = pnoise2((x + offset_x) * zoom / 50.0, (y + offset_y) * zoom / 50.0, octaves=3)
@@ -42,36 +41,48 @@ def draw_world(offset_x, offset_y, zoom):
 zoom = 1.0
 offset_x, offset_y = 0, 0
 move_speed = 10
+world_surface = draw_world(offset_x, offset_y, zoom)
 
 running = True
+world_needs_redraw = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mx, my = pygame.mouse.get_pos()
-            if event.button == 4:  # Mouse wheel up
+            if event.button == 4:
                 zoom += 0.1
                 offset_x -= (mx - SCREEN_WIDTH // 2) * 0.1
                 offset_y -= (my - SCREEN_HEIGHT // 2) * 0.1
-            elif event.button == 5:  # Mouse wheel down
+                world_needs_redraw = True
+            elif event.button == 5:
                 zoom -= 0.1
                 offset_x += (mx - SCREEN_WIDTH // 2) * 0.1
                 offset_y += (my - SCREEN_HEIGHT // 2) * 0.1
+                world_needs_redraw = True
             if zoom < 0.1:
                 zoom = 0.1
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
         offset_y -= move_speed
+        world_needs_redraw = True
     if keys[pygame.K_DOWN]:
         offset_y += move_speed
+        world_needs_redraw = True
     if keys[pygame.K_LEFT]:
         offset_x -= move_speed
+        world_needs_redraw = True
     if keys[pygame.K_RIGHT]:
         offset_x += move_speed
+        world_needs_redraw = True
 
-    screen.blit(draw_world(offset_x, offset_y, zoom), (0, 0))
+    if world_needs_redraw:
+        world_surface = draw_world(offset_x, offset_y, zoom)
+        world_needs_redraw = False
+
+    screen.blit(pygame.transform.scale(world_surface, (SCREEN_WIDTH, SCREEN_HEIGHT)), (0, 0))
     pygame.display.flip()
     clock.tick(60)
 
