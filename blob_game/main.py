ec7271042af51f1f94d4b61d944d53c920c4266f
hristo-darlyanov <83130557+hristo-darlyanov@users.noreply.github.com>
2023-10-08 13:11:19 +0300

    Added Food class for generating random food on screen

--- blob_game/main.py
+++ blob_game/main.py
@@ -8,6 +8,28 @@ WINDOW_HEIGHT   = 750
 WINDOW_SURFACE  = pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE
 GREY_COLOR = (122, 122, 122) # used for background
 
+class Food(pygame.sprite.Sprite):
+    def __init__( self, x, y, size):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.size = size
+        self.image = self.generate_food()
+        self.rect = self.image.get_rect()
+        self.rect.center = ( x, y )
+        self.position = pygame.math.Vector2( x, y )
+        
+    def generate_food(self):
+        if self.size == 5:
+            food_image  = pygame.transform.scale_by(pygame.image.load( 'food_size_5_32.png' ), 2).convert_alpha()
+        
+        # TODO: add other food size 1 trough 4
+            
+        return food_image
+        
+    def update(self):
+        self.rect.center = (self.position[0], self.position[1])
+    
+
 ### initialisation
 pygame.init()
 pygame.mixer.init()
@@ -20,6 +42,9 @@ black_blob = Blob( 300, 400, heading=5 )
 blob_sprites = pygame.sprite.Group() #Single()
 blob_sprites.add( black_blob )
 
+food = Food(350, 450, 5)
+food_sprites = pygame.sprite.Group()
+food_sprites.add(food)
 
 ### Main Loop
 clock = pygame.time.Clock()
@@ -53,10 +78,12 @@ while not done:
 
     # Update the blob(s)
     blob_sprites.update()
+    food_sprites.update()
 
     # Update the window
     window.fill(GREY_COLOR) # backgorund
     blob_sprites.draw( window )
+    food_sprites.draw( window )
     pygame.display.flip()
 
     # Clamp FPS
