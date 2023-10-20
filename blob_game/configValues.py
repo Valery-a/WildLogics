7aa90c27d2da6d9e0c82d580cfce4afbc8110f43
Valery-a <ValeryKulchicki@gmail.com>
2023-10-20 20:00:21 +0300

    moved the constants and values to a configValues

---
+++ blob_game/configValues.py
@@ -0,0 +1,26 @@
+import pygame
+import pymunk
+from pymunk.pygame_util import DrawOptions
+
+MINIMAP_WIDTH = 200
+MINIMAP_HEIGHT = 150
+GUI_PANEL_WIDTH = 200
+GUI_PANEL_COLOR = (50, 50, 50)
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 750
+BORDER_WIDTH = 10
+GREY_COLOR = (122, 122, 122)
+DISPLAY_WIDTH = WINDOW_WIDTH + 2 * BORDER_WIDTH
+DISPLAY_HEIGHT = WINDOW_HEIGHT + 2 * BORDER_WIDTH
+ZOOM_FACTOR = 1
+
+window = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+space = pymunk.Space()
+space.gravity = (0, 0)
+space.damping = 0.001
+draw_options = DrawOptions(window)
+gui_panel_visible = False
+background_surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+gui_panel_surface = pygame.Surface((GUI_PANEL_WIDTH, DISPLAY_HEIGHT))
+minimap_surface = pygame.Surface((MINIMAP_WIDTH, MINIMAP_HEIGHT))
